202236b554e230ead2f82688fba9dd8927bfde2b
Stanley Huang <huang.stanley050@gmail.com>
2020-01-11 17:58:03 +1100

    added some script to build react app

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,7 @@ WORKDIR '/app'
 COPY ./package.json .
 RUN npm install
 COPY . .
+CD ./client
+RUN npm install && npm build
+CD ../
 CMD ["npm","run","start"]
--- server/app.js
+++ server/app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("/", (req, res) => res.send("hi this is root"));
 app.get("/api", (req, res) => res.send("this is api route"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(`${__dirname}../client/build/index.html`));
+});
 
 module.exports = app;
